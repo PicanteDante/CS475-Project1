530924884e1e952743ab7866c34f652d007ea978
Dante <notapirate69yarhar@gmail.com>
2024-05-14 03:59:38 +0000

    program epicly segfaults woohoo

--- loop.bash
+++ loop.bash
@@ -1,9 +1,12 @@
 #!/bin/bash
-for t in 1 2 4 6 8
+
+# Array sizes: Choose a range of sizes from 1K to 8M.
+# Example sizes: 1024 (1K), 2048, 4096 (4K), 16384 (16K), 65536 (64K), 262144 (256K), 1048576 (1M), 2097152 (2M), 4194304 (4M), 8388608 (8M)
+
+for n in 1024 2048 4096 16384 65536 262144 1048576 2097152 4194304 8388608
 do
-  for n in 2 3 4 5 10 15 20 30 40 50
-  do
-     g++   main.cpp  -DNUMT=$t -DNUMCAPITALS=$n  -o proj03  -lm  -fopenmp
-    ./proj03
-  done
+   echo "Running tests with ARRAYSIZE=$n"
+   # Assuming 'main.cpp' is your source file:
+   g++ main2.cpp -DARRAYSIZE=$n -o simd_test -lm -fopenmp
+   ./simd_test
 done
